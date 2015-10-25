e0c38fb0a3f70d0395fea88a99ce9725e1e467e8
Nathalie van Hees <n_van_hees@hotmail.com>
2015-10-25 14:09:46 +0100

    Answer programming assignment 2
    This code is for programming assignment 2, R programming, Johns Hopkins
    on Coursera
    Written by Nathalie van Hees, 25 October 2015.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code is for programming assignment 2, R programming, Johns Hopkins on Coursera
+## Answers by Nathalie van Hees, 25 October 2015.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## The function makeCacheMatrix creates a matrix object that can cache its inverse.
+ 
+makeCacheMatrix <- function(x = matrix()){
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) i <<- solve
+    getinverse <- function() i
+    list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the matrix object returned by makeCacheMatrix.
+## If a previously calculated inverse matrix was stored in the cache by makeCacheMatrix,
+## cacheSolve retrieves and returns the cached matrix, including a message. 
+## If the cache is empty, cacheSolve calculates the inverse matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+    i <- x$getinverse()
+    if(!is.null(i)){
+        message("getting cached data")
+        return(i)
+    }
+    else
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
+
+## These lines will help you test the code in one go. 
+## Just remove the "##" marking the code below here as comments and run this whole page.
+
+## a <- matrix(rnorm(1:16),2,2)
+## b <- makeCacheMatrix(a)
+## cacheSolve(b)
+## cacheSolve(b)
